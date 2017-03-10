f262f265bd9110f98703b2111cd1c0ba58393933
bathuluri <bathuluri@gmail.com>
2017-03-10 01:01:45 +0000

    added multiplication

---
+++ src/Multiplication.java
@@ -0,0 +1,17 @@
+
+public class Multiplication {
+
+	public static void main(String[] args) {
+	int g = Multiply(5, 6);
+    System.out.println("Multiplication result is " +g);
+	
+	}
+
+
+	public static int Multiply(int a, int b) {
+		int c = a*b;
+		return c;
+	}
+
+	
+}
